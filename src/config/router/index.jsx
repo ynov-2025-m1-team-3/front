94ac9dad9ccce20ee80efcb2b982f3b3edd3ca4e
Tarumii <yhicher@gmail.com>
2025-04-17 16:27:16 +0200

    feat/creation des pages login et register + routes

--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Homepage from "../../views/homepage";
+import Login from "../../views/auth/login";
+import Register from "../../views/auth/register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  }
+
 ]);
 
 const Routes = () => {
---
+++ src/views/auth/login/index.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+      <div>
+        <h1>Welcome to the Login</h1>
+        <p>This is the Login of our application.</p>
+      </div>
+    );
+  }
+  export default Login;
