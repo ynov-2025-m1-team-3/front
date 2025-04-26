546dbf0f6ea4c4ee20817af73f4b6ad60986514f
ThomasGrare <thomasgrare64@gmail.com>
2025-04-27 00:53:11 +0200

    feat/fix(register): register hook + fix component

---
+++ src/hooks/useRegister.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import api from "@lib/fetch";
+
+const useRegister = () => {
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [confirmPassword, setConfirmPassword] = useState("");
+   const [error, setError] = useState("");
+
+   const handleSubmit = async (event) => {
+      event.preventDefault();
+      if (!firstName || !lastName || !email || !password || !confirmPassword) {
+        setError("Tous les champs sont requis!");
+      } else if (password !== confirmPassword) {
+        setError("Les mots de passe ne correspondent pas!");
+      } else {
+        setError("");
+      }
+      const response = await api.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+      console.log("response", response);
+   }
+
+   return {
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail, 
+    password,
+    setPassword,
+    confirmPassword,
+    setConfirmPassword,
+    error,
+    setError,
+    name,
+    setName,
+    handleSubmit
+   }
+}
+
+export default useRegister;
