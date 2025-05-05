5e802004d0e81e70b46d987e90a0179ded481b30
Tarumii <yhicher@gmail.com>
2025-05-05 14:39:15 +0200

    feature/creation-btn-sidedrawer-navbar

---
+++ src/components/SideDrawer/index.jsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import CottageIcon from "@mui/icons-material/Cottage";
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const drawerItems1 = [
+    { text: "Welcome", icon: <CottageIcon />, route: "/welcome" },
+    { text: "Login", icon: <LoginIcon />, route: "/login" },
+    { text: "Register", icon: <PersonAddIcon />, route: "/register" },
+  ];
+
+  const drawerItems2 = [
+    { text: "Upload JSON", icon: <UploadFileIcon />, route: "/upload" },
+    { text: "Dashboard", icon: <DashboardIcon />, route: "/dashboard" },
+    { text: "FeedbackViewer", icon: <ChatBubbleIcon />, route: "/feedback" },
+  ];
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation">
+      <List>
+        {drawerItems1.map(({ text, icon, route }) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              onClick={() => {
+                navigate(route);
+                setOpen(false);
+              }}
+            >
+              <ListItemIcon sx={{ color: "#20b4dc" }}>{icon}</ListItemIcon>
+              <ListItemText primary={text} sx={{ color: "#20b4dc" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {drawerItems2.map(({ text, icon, route }) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              onClick={() => {
+                navigate(route);
+                setOpen(false);
+              }}
+            >
+              <ListItemIcon sx={{ color: "#20b4dc" }}>{icon}</ListItemIcon>
+              <ListItemText primary={text} sx={{ color: "#20b4dc" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box>
+      <IconButton
+        onClick={toggleDrawer(true)}
+        sx={{ color: "#20b4dc", ml: 1 }}
+        edge="start"
+        aria-label="menu"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </Box>
+  );
+}
