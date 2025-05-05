bcd60a572a695da0743a7617b898f5db2b675117
ThomasGrare <thomasgrare64@gmail.com>
2025-05-05 12:45:08 +0200

    Retour PR alias import

--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Homepage from "../../views/homepage";
-import Uploadpage from "../../views/uploadJSON";
-import Login from "../../views/auth/login";
-import Register from "../../views/auth/register";
-import PrivateRoute from "../../components/PrivateRoute";
-import Landingpage from "../../views/landingpage";
+import Homepage from "@views/homepage";
+import Uploadpage from "@views/uploadJSON";
+import Login from "@views/auth/login";
+import Register from "@views/auth/register";
+import PrivateRoute from "@components/PrivateRoute";
+import Landingpage from "@views/landingpage";
 
 const router = createBrowserRouter([
   {
