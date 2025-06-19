a5621a5339dc442ee46f07cc4c97ff7d462b5ac1
Alley-eddine <alleybenhassine@gamil.com>
2025-06-19 17:11:18 +0200

    fix(ci): replace missing logo.png imports to fix Vite build

--- src/views/uploadJSON/index.jsx
+++ src/views/uploadJSON/index.jsx
@@ -1,27 +1,37 @@
 import Jsonpage from "../../components/uploadJson";
 import { Typography, Box } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import logo from "../../assets/logo.png";
 import SideDrawer from "../../components/SideDrawer";
 const Uploadpage = () => {
   return (
     <Box>
-      <Box
-         sx={{ position: "fixed", top: 16, left: 16, zIndex: 1300,}}>
+      <Box sx={{ position: "fixed", top: 16, left: 16, zIndex: 1300 }}>
         <SideDrawer />
-       </Box>
-
-      <img src={logo} alt="Logo" style={{ width: "200px", margin: "20px auto", display: "block" }} />
+      </Box>
+      <img
+        src="/logo.webp"
+        alt="Logo"
+        loading="lazy"
+        style={{ width: "200px", margin: "20px auto", display: "block" }}
+      />
       <Typography
         variant="h4"
         align="center"
-        sx={{ mt: 4, mb: 2, color: "#20b4dc", fontWeight: "bold"  ,fontFamily: "Roboto, sans-serif" }}
+        sx={{
+          mt: 4,
+          mb: 2,
+          color: "#20b4dc",
+          fontWeight: "bold",
+          fontFamily: "Roboto, sans-serif",
+        }}
       >
         Bienvenue sur FeedPulse
       </Typography>
-      
 
-      <AccountCircleIcon color="primary" sx={{color: "#20b4dc", fontSize: 48, mb: 2 }} />
+      <AccountCircleIcon
+        color="primary"
+        sx={{ color: "#20b4dc", fontSize: 48, mb: 2 }}
+      />
       {/* Ajouter l'username */}
 
       <Typography
@@ -29,8 +39,9 @@ const Uploadpage = () => {
         align="center"
         sx={{ mb: 4, color: "#333", fontFamily: "Roboto, sans-serif" }}
       >
-        Téléchargez votre fichier JSON pour commencer à utiliser notre application.
-        </Typography>
+        Téléchargez votre fichier JSON pour commencer à utiliser notre
+        application.
+      </Typography>
 
       <Jsonpage />
     </Box>
